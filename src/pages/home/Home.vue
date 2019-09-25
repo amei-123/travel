3a0ded328cd0a691df7ae293fae444c03756c2cb
amei-123 <1652804865@qq.com>
2019-09-25 15:46:48 +0800

    'project初始化'

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div class="home">home</div>
+        <router-link to='/list'>点击跳转</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+  .home{
+    font-size: 25px
+  }
+</style>
