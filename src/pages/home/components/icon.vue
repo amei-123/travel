af1163e50559d0c6cc35ed12d2999565627ef611
amei-123 <1652804865@qq.com>
2019-10-06 22:17:49 +0800

    'icon子分支'

---
+++ src/pages/home/components/icon.vue
@@ -0,0 +1,72 @@
+<template>
+    <swiper >
+        <swiper-slide v-for="(item,index) in pages" :key="index" class="icon-container">
+            <div class="icon-item" v-for="img in item" :key="img.id">
+                <img src="https://s.qunarzz.com/vacation_react/around/entry5.png" class="icon-img">
+                <p class="icon-detail">{{img.detail}}</p>
+            </div>
+        </swiper-slide>
+    </swiper>
+</template>
+
+<script>
+export default {
+    name:'HomeIcons',
+    data(){
+        return{
+            iconArr:[
+                {id:'001',url:'https://s.qunarzz.com/vacation_react/around/entry5.png',detail:'游遍全球游遍全球'},
+                {id:'002',url:'https://s.qunarzz.com/vacation_react/around/entry5.png',detail:'游遍全球'},
+                {id:'003',url:'https://s.qunarzz.com/vacation_react/around/entry5.png',detail:'游遍全球'},
+                {id:'004',url:'https://s.qunarzz.com/vacation_react/around/entry5.png',detail:'游遍全球'},
+                {id:'005',url:'https://s.qunarzz.com/vacation_react/around/entry5.png',detail:'游遍全球'},
+                {id:'006',url:'https://s.qunarzz.com/vacation_react/around/entry5.png',detail:'游遍全球'},
+                {id:'007',url:'https://s.qunarzz.com/vacation_react/around/entry5.png',detail:'游遍全球'},
+                {id:'008',url:'https://s.qunarzz.com/vacation_react/around/entry5.png',detail:'游遍全球'},
+                {id:'009',url:'https://s.qunarzz.com/vacation_react/around/entry5.png',detail:'游遍全球'}
+            ]
+        }
+    },
+    computed: {
+        pages () {  //分页实现轮播
+            let pages = [];
+            this.iconArr.forEach((item,index)=>{
+                var p = Math.floor(index/8);
+                if(!pages[p]){
+                    pages[p] = [];
+                }
+                pages[p].push(item);
+            })
+            return pages;
+        }
+    },
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/styles.styl'
+    .icon-container
+        overflow :hidden;
+        display :flex;
+        flex-wrap :wrap;
+        justify-content :space-between;
+        width :100%;
+        
+        .icon-item
+            display :flex;
+            justify-content :center;
+            align-items:center;
+            flex-wrap :wrap;
+            width :25%;
+            box-sizing :border-box;
+            padding:0 10px;
+            .icon-img
+                width:1.2rem;
+                height:1.2rem
+            .icon-detail
+                font-size:12px;
+                color:$textColor;
+                overflow :hidden;
+                text-overflow :ellipsis;
+                white-space :nowrap;
+</style>
--- src/pages/home/components/swiper.vue
+++ src/pages/home/components/swiper.vue
@@ -1,9 +1,12 @@
 <template>
-  <swiper :options="swiperOption">
-    <swiper-slide>I'm Slide 1</swiper-slide>
-    <swiper-slide>I'm Slide 2</swiper-slide>
-    <div class="swiper-pagination"  slot="pagination"></div>
-  </swiper>
+  <div class="img-container">
+    <swiper :options="swiperOption" >
+      <swiper-slide v-for="item in imgArr" :key="item.id">
+        <img :src="item.url" class="swiper-img">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
 </template>
 
 <script>
@@ -12,14 +15,40 @@ export default {
     data() {
       return {
         swiperOption: {
+            pagination:'.swiper-pagination',
             loop:true,
-            cancelable:true
-        }
+            // cancelable:true
+        },
+        imgArr:[
+          {
+            id:'001',
+            url:'https://img1.qunarzz.com/order/comp/1805/2e/6e407f088bfb902.png'
+          },
+          {
+            id:'002',
+            url:'https://img1.qunarzz.com/order/comp/1805/2e/6e407f088bfb902.png'
+          },
+          {
+            id:'003',
+            url:"https://img1.qunarzz.com/order/comp/1805/2e/6e407f088bfb902.png"
+          }
+        ]
       }
     },
 }
 </script>
 
 <style lang="stylus" scoped>
-@import '~styles/styles.styl';    
+  .img-container >>> .swiper-pagination-bullet-active 
+    background-color :$backgroundColor !important;
+  .img-container{
+    overflow :hidden;
+    width :100%;
+    height :0;
+    padding-bottom :40%;
+    .swiper-img{
+      width :100%;
+      height :100%;
+    }
+  }
 </style>
